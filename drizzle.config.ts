a0d39c1f385219e76ef899d54a1f2ff9af487d3e
Christian Kopac <hello@christiankopac.com>
2024-11-16 16:41:25 +0100

    chore: fix drizzle types

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   dialect: 'turso',
   dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN,
+    url: process.env.TURSO_DATABASE_URL || '',
+    authToken: process.env.TURSO_AUTH_TOKEN || '',
   },
 });
